8e611f050a49cd083070d288a0427faf7fee1877
omoikheloa <142723098+omoikheloa@users.noreply.github.com>
2024-06-14 11:38:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
+const imgs = document.getElementById('imgs');
+const img = document.querySelectorAll('#imgs img');
+
+let idx = 0;
+let interval = setInterval(run, 2000);
+
+function run() {
+    idx++;
+    changeImage();
+}
+
+function changeImage() {
+    if (idx > img.length - 1) {
+        idx = 0;
+    } else if (idx < 0) {
+        idx = img.length - 1;
+    }
+
+    imgs.style.transform = `translateX(${-idx * 300}px)`;
+}
+
+function resetInterval() {
+    clearInterval(interval);
+    interval = setInterval(run, 9000);
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-    const imgs = document.getElementById('imgs');
-    const img = document.querySelectorAll('#imgs img');
-    const playButton = document.getElementById('playButton');
-    const pauseButton = document.getElementById('pauseButton');
-    const backgroundMusic = document.getElementById('background-music');
-    let idx = 0;
-    let interval = setInterval(run, 2000);
-
-    function run() {
-        idx++;
-        changeImage();
-    }
-
-    function changeImage() {
-        if (idx > img.length - 1) {
-            idx = 0;
-        } else if (idx < 0) {
-            idx = img.length - 1;
-        }
-
-        imgs.style.transform = `translateX(${-idx * 300}px)`;
-    }
-
-    function resetInterval() {
-        clearInterval(interval);
-        interval = setInterval(run, 9000);
-    }
-
-    const albumArts = document.querySelectorAll('.album-art');
-    const popup = document.getElementById('albumPopup');
-    const close = document.querySelector('.popup-modal .close');
+    var albumArts = document.querySelectorAll('.album-art');
+    var popup = document.getElementById('albumPopup');
+    var close = document.querySelector('.popup-modal .close');
 
     albumArts.forEach(function(art) {
         art.addEventListener('click', function() {
@@ -47,14 +45,13 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    var backgroundMusic = document.getElementById('background-music');
     backgroundMusic.play().catch(function(error) {
         console.log('Playback failed: ' + error);
     });
 
     playButton.addEventListener('click', function() {
-        backgroundMusic.play().catch(function(error) {
-            console.log('Playback failed: ' + error);
-        });
+        backgroundMusic.play();
         playButton.style.display = 'none';
         pauseButton.style.display = 'inline';
     });
@@ -64,3 +61,4 @@ document.addEventListener('DOMContentLoaded', function() {
         playButton.style.display = 'inline';
         pauseButton.style.display = 'none';
     });
+});
